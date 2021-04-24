9df038cc7595d5a21ae1a4ad6116eade9bacee5c
Ivana Klementová <klementova.vf@gmail.com>
2021-04-24 13:00:55 +0200

    úkol-4

--- src/index.js
+++ src/index.js
@@ -15,3 +15,19 @@ for (let i = i; i < navLinkElm.length; i++) {
     navElm.classList.toggle('nav-closed');
   });
 }
+/*úkol 4*/
+const orderBtnElm = document.querySelector('#order-btn');
+const drinkCupElm = document.querySelector('.drink__cup');
+let ordered = false;
+
+orderBtnElm.addEventListener('click', () => {
+  if (!ordered) {
+    drinkCupElm.classList.add('drink__cup--selected');
+    orderBtnElm.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    drinkCupElm.classList.remove('drink__cup--selected');
+    orderBtnElm.textContent = 'Objednat';
+    ordered = false;
+  }
+});
